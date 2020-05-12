eefaff96063b7f785351839d25f8b4ca1406eec1
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2020-05-12 16:43:43 +0200

    fill in administrative info in setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,15 @@ from setuptools import setup, find_packages
 setup(
     name="bidule",
     packages=find_packages(),
+
+    # see https://semver.org/
+    # and https://pypi.org/project/semver/
+    version="0.0.1",
+
+    author="Thierry Parmentelat",
+    author_email="thierry.parmentelat@inria.fr",
+    description="A micro Python package to demo good practices",
+    keywords=["Python", "package", "setup.py", "setuptools"],
+    url="https://github.com/flotpython/bidule",
+    license="Creative Commons BY-CC-ND 4.0",
 )
